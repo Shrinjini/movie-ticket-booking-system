66d097f89686ee22b671d2a7abb8bf66f4f33cfc
shrinjini <piul.scorpion@gmail.com>
2024-05-16 23:47:25 +0530

    This is ameded structure of the modules

--- movie-ticket-booking-system/src/main/java/com/sapiens/movieticketbookingsystem/MovieTicketBookingSystemApplication.java
+++ movie-ticket-booking-system/src/main/java/com/sapiens/movieticketbookingsystem/MovieTicketBookingSystemApplication.java
@@ -2,12 +2,25 @@ package com.sapiens.movieticketbookingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/movie-ticket-booking-service")
 public class MovieTicketBookingSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MovieTicketBookingSystemApplication.class, args);
 	}
+
+	@GetMapping("/main-module")
+	public String mainModule()
+	{
+		return "main module";
+	}
+
 
 }
